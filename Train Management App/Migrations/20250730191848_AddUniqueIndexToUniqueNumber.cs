b0c1a4365c608f189bc8753c72eda37cbbe84eef
Iamfitz <vuitsyk.a@gmail.com>
2025-07-30 22:30:16 +0300

    migration activities and actual snapshot

---
+++ Train Management App/Migrations/20250730191848_AddUniqueIndexToUniqueNumber.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Train_Management_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexToUniqueNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UniqueNumber",
+                table: "TrainComponents",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrainComponents_UniqueNumber",
+                table: "TrainComponents",
+                column: "UniqueNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TrainComponents_UniqueNumber",
+                table: "TrainComponents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UniqueNumber",
+                table: "TrainComponents",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
